51b703e4ae420f06ebaa9da1f0a9a2e83f33cdb7
D3c3453d <sergey.et02@gmail.com>
2024-06-05 00:34:54 +0300

    refactor: increased EnvTempController readability

--- Framework/Controllers/EnvTempController.cs
+++ Framework/Controllers/EnvTempController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using StardewValley;
 using Temperature.Framework.Data;
+using Temperature.Framework.Misc;
 using StardewValley.Locations;
 using SObject = StardewValley.Object;
 
@@ -14,47 +15,31 @@ namespace Temperature.Framework.Controllers
         private static float fluctuationScale = DefaultConsts.FluctuationScale;
         private static readonly Random rand = new();
 
-        private static float PixelToTile(int pixel)
-        {
-            return (float)pixel / Game1.tileSize;
-        }
 
-        private static float Distance(float aX, float aY, float bX, float bY)
+        private static void ApplyMines(ref float envTemp, int currentMineLevel)
         {
-            return MathF.Sqrt((aX - bX) * (aX - bX) + (aY - bY) * (aY - bY));
-        }
-
-        private static float ParabolaWithCentralExtremum(float x, float extremumValue, float boundaryValue, float boundary1, float boundary2)
-        {
-            return 4 * (boundaryValue - extremumValue) /
-            ((boundary1 - boundary2) * (boundary1 - boundary2)) *
-            (x - (boundary1 + boundary2) / 2) * (x - (boundary1 + boundary2) / 2) + extremumValue;
-        }
-
-        private static float StraightThroughPoint(float x, float slope, float pointX, float pointY)
-        {
-            return slope * (x - pointX) + pointY;
-        }
-
-        private static bool CheckIfItemIsActive(SObject obj, bool activeType = false)
-        {
-            //check if the object is a machine for crafting (eg. Furnace, Charcoal Kiln)
-            if (activeType)
+            switch (currentMineLevel)
             {
-                if (obj.MinutesUntilReady > 0 && obj.heldObject.Value != null) return true;
-                else return false;
+                case MineShaft.quarryMineShaft:
+                    envTemp = DefaultConsts.EnvTemp;
+                    break;
+                case >= MineShaft.bottomOfMineLevel:
+                    envTemp =
+                    CalcHelper.StraightThroughPoint(currentMineLevel, DefaultConsts.SkullCaveTempSlope, MineShaft.bottomOfMineLevel, DefaultConsts.EnvTemp);
+                    break;
+                case >= MineShaft.mineLavaLevel:
+                    envTemp =
+                    CalcHelper.StraightThroughPoint(currentMineLevel, DefaultConsts.LavaMineTempSlope, MineShaft.mineLavaLevel, DefaultConsts.EnvTemp);
+                    break;
+                case >= MineShaft.mineFrostLevel:
+                    envTemp =
+                    CalcHelper.ParabolaWithCentralExtremum(currentMineLevel, DefaultConsts.MinFrostMineTemp, DefaultConsts.MaxFrostMineTemp, MineShaft.mineFrostLevel, MineShaft.mineLavaLevel);
+                    break;
+                case > MineShaft.upperArea:
+                    envTemp =
+                    CalcHelper.ParabolaWithCentralExtremum(currentMineLevel, DefaultConsts.MaxUpperMineTemp, DefaultConsts.MinUpperMineTemp, MineShaft.upperArea, MineShaft.mineFrostLevel);
+                    break;
             }
-            else
-            {
-                //if not a machine for crafting (assuming furniture), check if said furniture is active
-                if (obj.IsOn) return true;
-                else return false;
-            }
-        }
-
-        private static float SeasonCycleTemp(float minTemp, float maxTemp, int totalDays)
-        {
-            return (maxTemp - minTemp) / 2 * MathF.Sin((totalDays - WorldDate.DaysPerMonth / 2) * MathF.PI / (2 * WorldDate.DaysPerMonth)) + (maxTemp + minTemp) / 2;
         }
 
         private static void ApplySeasonCycleTemp(ref float envTemp, EnvModifiers envData, int totalDays)
@@ -62,13 +47,13 @@ namespace Temperature.Framework.Controllers
             switch (envData.MinValue, envData.MaxValue)
             {
                 case ( > DefaultConsts.AbsoluteZero, > DefaultConsts.AbsoluteZero):
-                    envTemp = SeasonCycleTemp(envData.MinValue, envData.MaxValue, totalDays);
+                    envTemp = CalcHelper.SinWithMinMax(totalDays, envData.MinValue, envData.MaxValue, WorldDate.DaysPerMonth / 2, WorldDate.DaysPerMonth * 2);
                     break;
                 case ( > DefaultConsts.AbsoluteZero, DefaultConsts.AbsoluteZero):
-                    envTemp = SeasonCycleTemp(envData.MinValue, envData.MinValue, totalDays);
+                    envTemp = CalcHelper.SinWithMinMax(totalDays, envData.MinValue, envData.MinValue, WorldDate.DaysPerMonth / 2, WorldDate.DaysPerMonth * 2);
                     break;
                 case (DefaultConsts.AbsoluteZero, > DefaultConsts.AbsoluteZero):
-                    envTemp = SeasonCycleTemp(envData.MaxValue, envData.MaxValue, totalDays);
+                    envTemp = CalcHelper.SinWithMinMax(totalDays, envData.MaxValue, envData.MaxValue, WorldDate.DaysPerMonth / 2, WorldDate.DaysPerMonth * 2);
                     break;
             }
         }
@@ -103,32 +88,6 @@ namespace Temperature.Framework.Controllers
             fluctuationScale += envData.FluctuationScale;
         }
 
-        private static void ApplyMines(ref float envTemp, int currentMineLevel)
-        {
-            switch (currentMineLevel)
-            {
-                case MineShaft.quarryMineShaft:
-                    envTemp = DefaultConsts.EnvTemp;
-                    break;
-                case >= MineShaft.bottomOfMineLevel:
-                    envTemp =
-                    StraightThroughPoint(currentMineLevel, DefaultConsts.SkullCaveTempSlope, MineShaft.bottomOfMineLevel, DefaultConsts.EnvTemp);
-                    break;
-                case >= MineShaft.mineLavaLevel:
-                    envTemp =
-                    StraightThroughPoint(currentMineLevel, DefaultConsts.LavaMineTempSlope, MineShaft.mineLavaLevel, DefaultConsts.EnvTemp);
-                    break;
-                case >= MineShaft.mineFrostLevel:
-                    envTemp =
-                    ParabolaWithCentralExtremum(currentMineLevel, DefaultConsts.MinFrostMineTemp, DefaultConsts.MaxFrostMineTemp, MineShaft.mineFrostLevel, MineShaft.mineLavaLevel);
-                    break;
-                case > MineShaft.upperArea:
-                    envTemp =
-                    ParabolaWithCentralExtremum(currentMineLevel, DefaultConsts.MaxUpperMineTemp, DefaultConsts.MinUpperMineTemp, MineShaft.upperArea, MineShaft.mineFrostLevel);
-                    break;
-            }
-        }
-
         private static void ApplyLocation(ref float envTemp, GameLocation location, int totalDays, int currentMineLevel)
         {
             var envData = DataController.Locations.Data.GetValueOrDefault(location.Name) ??
@@ -138,7 +97,7 @@ namespace Temperature.Framework.Controllers
             if (location.Name == DefaultConsts.MineName + currentMineLevel)
             {
                 ApplyMines(ref envTemp, currentMineLevel);
-                envData.DayCycleScale = 0;
+                envData.DayCycleScale = 0; // there is no day cycle income in mines
             }
             else if (!location.IsOutdoors)
             {
@@ -177,15 +136,15 @@ namespace Temperature.Framework.Controllers
                             nearbyObjects.Add(obj);
 
                             // skip inactive objects, that need to be activated
-                            if (objectData.NeedActive && !CheckIfItemIsActive(obj, objectData.ActiveType)) continue;
+                            if (objectData.NeedActive && !CalcHelper.CheckIfItemIsActive(obj, objectData.ActiveType)) continue;
 
                             // prioritize ambient temp if it exceed device's core temp
                             if ((objectData.DeviceType == DefaultConsts.HeatingType && objectData.CoreTemp < envTemp) ||
                                 (objectData.DeviceType == DefaultConsts.CoolingType && objectData.CoreTemp > envTemp))
                                 continue;
 
-                            // dealing with target temp this.value here?
-                            float dist = MathF.Max(Distance(PixelToTile(obj.GetBoundingBox().Center.X), PixelToTile(obj.GetBoundingBox().Center.Y), playerTileX, playerTileY), 1);
+                            // dealing with env temp
+                            float dist = MathF.Max(CalcHelper.Distance(CalcHelper.PixelToTile(obj.GetBoundingBox().Center.X), CalcHelper.PixelToTile(obj.GetBoundingBox().Center.Y), playerTileX, playerTileY), 1);
                             if (dist <= objectData.EffectiveRange)
                             {
                                 float tempModifierEntry = (objectData.CoreTemp - envTemp) / (15 * (dist - 1) / objectData.EffectiveRange + 1);
@@ -209,7 +168,7 @@ namespace Temperature.Framework.Controllers
                 {
                     ObjectModifiers objectData = DataController.Objects.Data.GetValueOrDefault(obj.Name);
                     if (objectData != null)
-                        if (!objectData.NeedActive || CheckIfItemIsActive(obj, objectData.ActiveType)) // skips inactive objects, that need to be activated
+                        if (!objectData.NeedActive || CalcHelper.CheckIfItemIsActive(obj, objectData.ActiveType)) // skips inactive objects, that need to be activated
                             objectsData.Add(objectData);
                 }
 
@@ -218,7 +177,7 @@ namespace Temperature.Framework.Controllers
                 {
                     ObjectModifiers objectData = DataController.Objects.Data.GetValueOrDefault(obj.Name);
                     if (objectData != null)
-                        if (!objectData.NeedActive || CheckIfItemIsActive(obj, objectData.ActiveType)) // skips inactive objects, that need to be activated
+                        if (!objectData.NeedActive || CalcHelper.CheckIfItemIsActive(obj, objectData.ActiveType)) // skips inactive objects, that need to be activated
                             objectsData.Add(objectData);
                 }
 
@@ -227,7 +186,7 @@ namespace Temperature.Framework.Controllers
 
                 foreach (ObjectModifiers objectData in objectsData)
                 {
-                    //calculate indoor heating power base on core temp and range (assume full effectiveness if object is placed indoor)
+                    //calculate indoor heating power base on core temp and range
                     if (objectData.DeviceType == DefaultConsts.GeneralType)
                     {
                         float perfectAmbientPower = area * DefaultConsts.EnvTemp;
@@ -246,7 +205,7 @@ namespace Temperature.Framework.Controllers
         private static void ApplyDayCycle(ref float envTemp, int timeOfDay)
         {
             float hours = timeOfDay / 100 + timeOfDay % 100 / 60.0f; // converting ingame "digital clock" time to irl hours with floating point
-            envTemp += MathF.Sin((hours - DefaultConsts.DayCycleOffset) * MathF.PI / DefaultConsts.DayCycleStretch) * dayCycleScale;
+            envTemp += CalcHelper.SinWithMinMax(hours, -dayCycleScale, dayCycleScale, DefaultConsts.DayCycleOffset, DefaultConsts.DayCycleStretch);
         }
 
         public static float Update()
@@ -262,7 +221,7 @@ namespace Temperature.Framework.Controllers
                 ApplySeason(ref envTemp, location, Game1.Date.TotalDays, Game1.getFarm().GetSeason().ToString());
                 ApplyWeather(ref envTemp, location, Game1.Date.TotalDays);
                 ApplyLocation(ref envTemp, location, Game1.Date.TotalDays, Game1.CurrentMineLevel);
-                ApplyObjects(ref envTemp, location, PixelToTile(Game1.player.GetBoundingBox().Center.X), PixelToTile(Game1.player.GetBoundingBox().Center.Y));
+                ApplyObjects(ref envTemp, location, CalcHelper.PixelToTile(Game1.player.GetBoundingBox().Center.X), CalcHelper.PixelToTile(Game1.player.GetBoundingBox().Center.Y));
                 ApplyAmbient(ref envTemp, location);
             }
             else
